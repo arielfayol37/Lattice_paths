29554e139d230cb9812ef79a6c75843f11033934
arielfayol37 <115123676+arielfayol37@users.noreply.github.com>
2022-11-28 21:39:43 -0600

    Update drawing_paths.py

--- drawing_paths.py
+++ drawing_paths.py
@@ -13,6 +13,10 @@ x_size, y_size = user32.GetSystemMetrics(0),user32.GetSystemMetrics(1)
 #s.setposition(x_or, y_or)
 #s.pendown()
 # method to draw y-axis lines
+colors = [ (60, 180, 75), (255, 225, 25),  (245, 130, 48),(170, 110, 40), \
+ (70, 240, 240),(145, 30, 180),  (128, 128, 128), (240, 50, 230), (210, 245, 60),(0, 130, 200), (250, 190, 212), (0, 128, 128),\
+ (220, 190, 255),  (255, 250, 200), (128, 0, 0), (170, 255, 195), (230, 25, 75),(128, 128, 0),\
+  (255, 215, 180), (0, 0, 128)]
 def drawy(x_forward,x_or, y_or, s):
 	
 	# line
@@ -65,7 +69,7 @@ def draw_lattice(m,n):
         #canvas.exitonClick()
         #s.close()
 #s.hideturtle()
-def draw_path(pat_A,m,n,o):
+def draw_path(pat_A,m,n,o,index):
         canvas = turtle.Screen()
         turtle.TurtleScreen._RUNNING=True        
         x_or =-0.5*x_size+200
@@ -74,8 +78,11 @@ def draw_path(pat_A,m,n,o):
         y_forward = (y_size-2*200)/n        
         s=turtle.Turtle()
         s.width(1.7)
-        s.color((random.random(),random.random(),random.random()))
-        s.speed(10)
+        try:
+                s.color(colors[index])
+        except:
+                s.color((random.random(),random.random(),random.random()))
+        s.speed(7)
         if pat_A[0]:
                 s.penup()
                 s.setposition(x_or-o, y_or+10-o)
