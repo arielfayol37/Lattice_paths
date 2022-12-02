146500ec8c47f01ed78a5028ed1423a20cf3e276
arielfayol37 <115123676+arielfayol37@users.noreply.github.com>
2022-12-01 23:45:01 -0600

    Update run.py

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
-import openpyxl
-#from pop_w_replace import Population
-from Population import Population
+from os import kill
+import openpyxl,time, concurrent.futures as cf
+from pop_w_replace import Population
+#from Population import Population
 from Sequence import Sequence
-from lp_utils import generate_all_paths
+from lp_utils import generate_all_paths, sume
 
 def run(m,n,k,mode="roulette",old_world=0,pop_size=1000):
     filename = "lattice_table_g" + str(n) + '.xlsx'
@@ -85,11 +86,15 @@ def collect_data(m,n):
             world=run(i,n,k,"roulette", pop_size = i**2 * 50)
 
 
-def test(size,j,m,n,k,mode="roulette",norm=True):
+def test(size,j,m,n,k,kill_mode="non_bias_random",mode="roulette",norm=True):
+    
+    start_time = time.perf_counter()
     world = Population(size,m,n,k,norm=norm)
     world.num_genes =j
-    best = world.evolve(mode)
-    best.draw()
+    best = world.evolve(mode,kill_mode)
+    end_time = time.perf_counter()
+    print(f"It took {(end_time-start_time)/60} minutes to find this solution")
+    #best.draw()
     world.visualize_evolution()
     return
 def greedy(m,n,k,pats):
@@ -107,17 +112,24 @@ def greedy(m,n,k,pats):
                 break
             if i == len(sol)-1:
                 sol.append(s)
-    return sol
-def greedy_test(m,n,k,paths,solutions = [],recurse=False):
-    
-     
-    solutions.append(greedy(m,n,k,paths))
+    #sol_num = [i for i in range(len(sol))]            
+    #return sol
+    return len(sol)
+def greedy_test(m,n,k,paths,recurse=False):
+    solutions = []
+    pati = paths.copy()
+    solutions.append(greedy(m,n,k,pati))
     for i in range(len(paths)):
         patos = paths.copy()
         patos.pop(i)
         if recurse:
-            solutions.append(greedy_test(m,n,k,patos))
+            patos2 = patos.copy()
+            
+            #patos.reverse()
+            solutions+=greedy_test(m,n,k,patos2,False)
+            #patos.reverse()
         assert len(patos) == len(paths)-1
+        
         solutions.append(greedy(m,n,k,patos))
     return solutions
 def find_max(alist):
@@ -137,14 +149,72 @@ def collect_data_greedy(m,n):
 
     for i in range(m,12):
         paths = generate_all_paths(i,n)
-        w = []
+        #w = []
         for k in range(1,i+n):
             sheet.cell(1,k+1).value =str(k)
             sheet.cell(i+1,1).value= str(i)
             f = greedy_test(i,n,k,paths,recurse=True)
-            for z in f:
-                w.append(len(z))
-            maxi = find_max(w)
+            
+            #for z in f:
+                #w.append(len(z))
+            maxi = find_max(f)
             sheet.cell(i+1,k+1).value=maxi
-            w = []
+            #w = []
             wb.save(filename)
+
+def nbrr(non_bias_random_times):
+
+    for i in range(10):
+         
+        start_time = time.perf_counter()
+        test(700,15,6,3,5,kill_mode="non_bias_random")
+        end_time = time.perf_counter()
+        non_bias_random_times.append((end_time-start_time)/60)
+         
+        #non_bias_random_times.append(10-i)
+        #print('Sindy')
+    return non_bias_random_times  
+       
+def kbr(kill_bottom_times):
+    
+    for i in range(10):
+        #kill_bottom_times.append(i)
+         
+        start_time = time.perf_counter()
+        test(700,15,6,3,5,kill_mode = "kill_bottom")
+        end_time = time.perf_counter()
+        kill_bottom_times.append((end_time-start_time)/60)
+             
+        #print("Ariel")
+    return kill_bottom_times 
+ 
+    
+def compare_kill_mode():
+    kill_bottom_times =[]
+    non_bias_random_times = []
+    kr = []
+    nr = []
+    with cf.ProcessPoolExecutor() as executor:
+        f1 =executor.submit(kbr,kill_bottom_times)
+        f2 =executor.submit(nbrr,non_bias_random_times)
+        kr = f1.result()
+        nr = f2.result()
+
+
+    """
+    p1 = mp.Process(target=nbrr, args = [non_bias_random_times])
+    p2 = mp.Process(target=kbr, args = [kill_bottom_times])
+    p1.start()
+    p2.start()
+
+    
+    p1.join()
+    p2.join()
+    """
+
+    #print("Kill_bottom_times:", kr)
+    #print("non_bias_random_times:", nr)
+    print("kr av: ", sume(kr)/len(kr))
+    print("nr av: ", sume(nr)/len(nr))
+    
+ 
