94d6e43525cd78e470dd6e20b08a5789caff9606
arielfayol37 <115123676+arielfayol37@users.noreply.github.com>
2022-12-06 18:06:31 -0600

    Update Genome.py

--- Genome.py
+++ Genome.py
@@ -1,121 +1,162 @@
+#This is Genome.py
+#python3 Genome.py
 from Sequence import Sequence
 from lp_utils import translate
 import random
 from drawing_paths import draw_path, draw_lattice
  
 class Genome():
-    def __init__(self, num_sequences, m, n, k, empty=False):
-        self.sequences = []
+    def __init__(self, num_sequences, m, n, k, paths, len_paths, empty=False):
+        # Store the values of num_sequences, m, n, and k.
+         
+        
         self.m = m
         self.n = n
         self.k = k
         self.num_sequences = num_sequences
+        self.sequences = [] # Initialize the list of sequences in the genome.
+         
         #self.poison()
+        self._generate_sequences(paths, len_paths, empty)
+
+
+    def _generate_sequences(self, paths, len_paths, empty):
+        # Generate a list of indices for the available paths.
+        self.take_paths = [i for i in range(len_paths)]
+        
+        # Generate the sequences in the genome based on the value of the empty parameter.
         if not empty:
-            for i in range(num_sequences):
-                new_seq = Sequence(self.m, self.n)
-                #while new_seq.same_paths(self.poison1) or new_seq.same_paths(self.poison2):
-                    #new_seq = Sequence(self.m, self.n)
+            for i in range(self.num_sequences):
+                # Choose a random index from the list of available paths.
+                r = random.randint(0, len(self.take_paths) - 1)
+                
+                # Create a new sequence using the chosen path.
+                new_seq = Sequence(self.m, self.n, paths=paths, index=self.take_paths[r])
+                
+                # Add the new sequence to the genome, and remove the chosen path from the list of available paths.
                 self.sequences.append(new_seq)
+                self.take_paths.pop(r)
         else:
-            for i in range(num_sequences):
-                new_seq = Sequence(self.m, self.n, True)
+    # If the empty parameter is True, generate empty sequences.
+            for i in range(self.num_sequences):
+                new_seq = Sequence(self.m, self.n, empty=True)
                 self.sequences.append(new_seq)
-            
+
                  
 
-    def fitness(self, penalty_indexes = True):
+    def fitness(self, penalty_indexes=True):
+        # Calculate the fitness of the genome.
         penalty = 0
         penalty_index = []
 
         if penalty_indexes:
+            # If the penalty_indexes parameter is True, calculate the penalty and the penalty index.
             for i in range(len(self.sequences)):
-                for j in range(i+1,len(self.sequences)):
-                    xo = self.sequences[i].compare(self.sequences[j], self.k) #returns 0 if two paths(sequences) are k-equivalent
+                for j in range(i + 1, len(self.sequences)):
+                    # Check if the sequences at the given indices are k-equivalent.
+                    xo = self.sequences[i].compare(self.sequences[j], self.k)
+                    
+                    # If the sequences are k-equivalent, increment the penalty and add the indices to the penalty index.
                     if xo == 0:
                         penalty += 1
-                        penalty_index.append([i,j])
+                        penalty_index.append([i, j])
 
-            if penalty ==0:#all paths are k-distinct (share at most (k-1) edges)
+            if penalty == 0:
+                # If the penalty is 0, return a large value and the empty penalty index.
                 return (9999, penalty_index)
 
-            return (1/penalty, penalty_index)
+            # Return the inverse of the penalty and the penalty index.
+            return (1 / penalty, penalty_index)
         else:
+            # If the penalty_indexes parameter is False, only calculate the penalty.
             for i in range(len(self.sequences)):
-                for j in range(i+1,len(self.sequences)):
-                    xo = self.sequences[i].compare(self.sequences[j], self.k) #returns 0 if two paths(sequences) are k-equivalent
+                for j in range(i + 1, len(self.sequences)):
+                    # Check if the sequences at the given indices are k-equivalent.
+                    xo = self.sequences[i].compare(self.sequences[j], self.k)
+                    
+                    # If the sequences are k-equivalent, increment the penalty.
                     if xo == 0:
                         penalty += 1
-            if penalty ==0:
+            
+            if penalty == 0:
+                # If the penalty is 0, return a large value.
                 return 9999
-            return 1/penalty                        
-    def divert(self,other): #calculate divergence from other set of solutions(Genome). Range of values [0,1]
-        different = 0
+            
+            # Return the inverse of the penalty.
+            return 1 / penalty
+
+    def divert(self, other):
+        # Calculate the divergence between the genome and another genome.
+        same = 0
         count = 0
         len_seq = len(self.sequences)
+        
+        # Compare each sequence in the genome to each sequence in the other genome.
         for i in range(len_seq):
             for j in range(len_seq):
-                different += self.sequences[i].same_paths(other.sequences[j]) #returns zero if different
-            if different == 0:
-                count+=1
-            else:
-                different = 0
-        return count/len_seq
+                # Check if the sequences at the given indices are the same.
+                same += self.sequences[i].same_paths(other.sequences[j])
             
-                        
+            if same == 0:
+                # If the sequences are different, increment the count.
+                count += 1
+            else:
+                # If the sequences are the same, reset the different variable.
+                same = 0
+        
+        # Return the ratio of sequences that are different.
+        return count / len_seq
+
     def show(self):
+        # Print the sequences in the genome to the console.
         for sequence in self.sequences:
             sequence.show()
             print("\n")
-    def mutate(self):
+    def mutate(self, paths):
+        # Mutate the genome by replacing one of its sequences with a new sequence.
+
+        # Calculate the penalty index.
         index_eq = self.fitness()[-1]
-         
-        if index_eq: #if some paths are k-equivalent
-            r = self.fitness()[-1][random.randint(0,len(index_eq)-1)][random.randint(0,1)]
-            self.sequences[r] = Sequence(self.m, self.n)
-        return self
-    def nmutate(self):
-        index_eq = self.fitness()[-1]
-         
+
+        # Choose a random index from the list of available paths.
+        i = random.randint(0, len(self.take_paths) - 1)
+
         if index_eq:
-            r= random.randint(0,self.num_sequences-1)
-            self.sequences[r] = Sequence(self.m, self.n)    
+            # If the penalty index is not empty, choose a random index from the penalty index.
+            r = index_eq[random.randint(0, len(index_eq) - 1)][random.randint(0, 1)]
+            
+            # Replace the sequence at the chosen index with a new sequence using the chosen path.
+            self.take_paths.append(self.sequences[r].pi)
+            self.sequences[r] = Sequence(self.m, self.n, paths=paths, index=self.take_paths[i])
+            self.take_paths.pop(i)
+
+        # Return the genome.
         return self
+
+        return self
+    def nmutate(self, paths):
+    # Mutate the genome by replacing multiple sequences with new sequences.
+    
+    # Calculate the penalty index.
+        index_eq = len(self.fitness()[-1])
+ 
+        if index_eq:
+             
+            r = random.randint(0,self.num_sequences-1)
+            
+             
+            self.take_paths.append(self.sequences[r].pi)
+            self.sequences[r] = Sequence(self.m, self.n, paths=paths, len_paths=len(paths))
+            self.take_paths.pop(self.sequences[r].pi) 
+        return self
+
     def translate(self):
         translated = []
         for sequence in self.sequences:
             translated.append(translate(sequence,"to_A"))
 
         return translated    
-
-    def poison(self):
-        patA = []
-        patB = []
-        if self.k<=self.m:
-            for i in range(self.k):
-                patA.append(0)
-            for i in range(self.n):
-                patA.append(1)
-                #patB.append(0)
-            for i in range(self.m - self.k):
-                patA.append(0)
-        else:
-            for s in range(self.m+self.n):
-                patA.append(1)
-        if self.k<=self.n:        
-            for i in range(self.k):
-                patB.append(1)
-            for i in range(self.m):
-                patB.append(0)
-                #patB.append(0)
-            for i in range(self.n - self.k):
-                patB.append(1)
-        else:
-            for s in range(self.m+self.n):
-                patB.append(1)
-                
-        self.poison2 = translate(patB, "to_O") 
-        self.poison1 = translate(patA, "to_O")
+ 
 
     def draw(self):
         draw_lattice(self.m,self.n)
