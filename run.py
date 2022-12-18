f026f77b3277ead866e3f47661c178226934d75f
arielfayol37 <115123676+arielfayol37@users.noreply.github.com>
2022-12-18 02:25:52 -0600

    Update run.py

--- run.py
+++ run.py
@@ -5,7 +5,10 @@ from Population import Population
 #from Population import Population
 from Sequence import Sequence
 from lp_utils import generate_all_paths
+from pebble import ProcessPool
 
+"""
+#you do not need to translate the run function, so you can skip this and o directly to collect_data 
 def run(m,n,k,mode="roulette",old_world=None,pop_size=1000):
     filename = "lattice_table_g" + str(n) + '.xlsx'
     try:
@@ -16,8 +19,7 @@ def run(m,n,k,mode="roulette",old_world=None,pop_size=1000):
     sheet.cell(1,1).value ="m/k"
     sheet.cell(1,k+1).value =str(k)
     sheet.cell(m+1,1).value= str(m)
-    j=1
-    world = Population(pop_size,m,n,k)
+     
  
     continuei = True
     if k>=n:
@@ -31,12 +33,7 @@ def run(m,n,k,mode="roulette",old_world=None,pop_size=1000):
         sheet.cell(m+1, k+1).value = j-1
         
     wb.save(filename)
-    if old_world:
-        old_world.k = k
-        for indi in old_world.individuals:
-            indi.k = k
-        world = old_world
-        j = old_world.num_genes
+ 
        
     while continuei:
         print("LET'SSSS GGOOOO")
@@ -46,13 +43,13 @@ def run(m,n,k,mode="roulette",old_world=None,pop_size=1000):
         if best.fitness()[-1]:
             continuei =False
         else:
-            """ #Do not be lazy
+             #Do not be lazy
             world.bsort()
             try:
                 assert best.fitness()[0] == world.fitnesses[0]
             except:
                 raise Exception("best individual not at index zero after sorting, line 52")
-            """    
+                
             new_world = Population(pop_size,m,n,k,create_paths=False)
             new_world.paths = world.paths
             new_world.l = world.l
@@ -70,58 +67,117 @@ def run(m,n,k,mode="roulette",old_world=None,pop_size=1000):
             wb.save(filename)
             if continuei == False:
                 break
-            """ #don't be lazy man. let it restart to avoid early convergence
+              #don't be lazy man. let it restart to avoid early convergence
             for i in range(0,int(0.1 * len(world.individuals))):
                 world.individuals[i].sequences.append(Sequence(m,n))
                 new_world.individuals.append(world.individuals[i])
-            """  
+             
             world = new_world
     wb.close()
     return world
-
+"""
 
 def collect_data(m,n):
-    for i in range(m,12):
-        world = run(i,n,3,"roulette") 
-        for k in range(3+1,i+n):
-            world=run(i,n,k,"roulette", pop_size = i**2 * 50)
-
-
-def test(size,j,m,n,k,kill_mode="non_bias_random",mode="roulette",norm=True, scale=True, draw=False, visualize=False):
-    
-    start_time = time.perf_counter()
-    world = Population(size,j,m,n,k,norm=norm, scale=scale)
-     
-    best = world.evolve(mode,kill_mode)
-    end_time = time.perf_counter()
-    print(f"It took {(end_time-start_time)/60} minutes to find this solution")
-    if draw:
-        best.draw()
-    if visualize:    
-        world.visualize_evolution()
-    return best
-
-def grab(m,n,k):
-    filename = "lattice_table_gen_" + str(n) + ".xlsx"
+    wb1 = openpyxl.load_workbook("lt_ds_rloo_" + str(n)+".xlsx")
+    wb2 = openpyxl.load_workbook("lt_ds_loo_" + str(n)+".xlsx")
+    filename = "lattice_table_genetic_" + str(n) + '.xlsx'
     try:
         wb = openpyxl.load_workbook(filename)
     except:
         wb = openpyxl.Workbook()
-    return wb["sheet"].cell(m+1,k+1)    
+    
+    for i in range(m,12):
+         
+        for k in range(3,i+n-1):
+            if wb1["sheet"].cell(i+1,k+1) > wb2["sheet"].cell(i+1,k+1):
+                target = wb1["sheet"].cell(i+1,k+1)
+            else:
+                target = wb2["sheet"].cell(i+1,k+1)   
+             
+            run = parallel_search(target=target,m=i,n=n,k=k)
+            while run:
+                target +=1
+                run = parallel_search(target=target,m=i,n=n,k=k)
+            wb["sheet"].cell(i+1,k+1).value = target-1
+            wb.save(filename)    
+    wb.close()
+    wb1.close()
+    wb2.close()
 
+def search(size,j,m,n,k,kill_mode="non_bias_random",mode="roulette",norm=True, scale=True, draw=False, visualize=False,temp=4.0):
+    
+     
+    world = Population(size,j,m,n,k,norm=norm, scale=scale, temp=temp)
+     
+    best = world.evolve(mode,kill_mode)
+     
+    
+    if draw:
+        best.draw()
+    if visualize:    
+        world.visualize_evolution()
+    return world
+def parallel_search(target,m,n,k):
+    # Set the parameter values for the function
+    
+    evolution_parameters = [
+         {'size': 1000, 'j': target, 'm': m, 'n': n, 'k': k, 'kill_mode': "non_bias_random",'temp':10},
+        {'size': 400, 'j': target, 'm': m, 'n': n, 'k': k, 'kill_mode': "non_bias_random",'temp':5},
+        {'size': 5000, 'j': target, 'm': m, 'n': n, 'k': k, 'kill_mode': "non_bias_random",'temp':3},
+        {'size': 20000, 'j': target, 'm': m, 'n': n, 'k': k, 'kill_mode': "kill_bottom",'temp':2},
+         
+    ]
+     
+ 
+    # Create a ProcessPoolExecutor object to run the function in parallel
+    #with cf.ProcessPoolExecutor() as executor:
+    with ProcessPool() as executor:
+        # Create a list of asynchronous tasks
+        tasks = []
+        for param in evolution_parameters:
+            task = executor.schedule(search, kwargs=param)
+            tasks.append(task)
 
-def srun():
-    list_kwargs = [] #fill this using a function to grab excel values
-    futures = []
+        # Wait for the tasks to complete
+        run = True
+        count = 0 
+        while run:
+            for i in range(len(tasks)):
+                if tasks[i].done():
+                    
+                    result = tasks[i].result()
+                    if result.fitnesses[result.bfi] == 9999:
+                        run = False
+                        config_index = i                        
+                        for other_task in tasks:
+    
+                            c=other_task.cancel()
+                             
+                        break
+            time.sleep(target*10)
+            if count !=0:
+                if result.fitnessees[result.bfi]!=9999:
+                    if tasks[-1].done():
+                        #merge all populations
+                        new_pop =tasks[0].result()
+                        for task in tasks[1:]:
+                            new_pop.individuals.append(task.result().individuals)
+                            new_pop.max_size = 4000
+                            new_pop.av_pop_fitnesses.clear()
+                            new_pop.av_pop_divergences.clear()
+                        new_best = new_pop.evolve(mode="roulette",kill_mode = "non_bias_random")    
+                        if new_pop.fitnesses[new_pop.bfi]==9999:
+                            result = new_pop
+                      
+    result.individuals[result.bfi].show(result.paths)
+    f = result.fitnesses[result.bfi]
+    print("BestFitness:", f, "Config_index: ", config_index)    
+    if f== 9999:
 
-    with cf.ProcessPoolExecutor as executor:
-        for i in len(list_kwargs):
-            futures.append(executor.submit(target=test, **list_kwargs[i]))
-        while True:
-            for f in cf.as_completed(futures):
-                if f.done():
-                    for fu in futures:
-                        fu.cancel()
+        return True
+    else:
+        return False    
+ 
 
 def greedy(m,n,k,pats):
     sol = []
@@ -258,3 +314,17 @@ def compare_kill_mode():
     print("nr av: ", sum(nr)/len(nr))
     
  
+ 
+def test(size,j,m,n,k,kill_mode="non_bias_random",mode="roulette",norm=True, scale=True, draw=False, visualize=False,temp=4.0):
+    
+    start_time = time.perf_counter()
+    world = Population(size,j,m,n,k,norm=norm, scale=scale, temp=temp)
+     
+    best = world.evolve(mode,kill_mode)
+    end_time = time.perf_counter()
+    print(f"It took {(end_time-start_time)/60} minutes to find this solution")
+    if draw:
+        best.draw()
+    if visualize:    
+        world.visualize_evolution()
+    return  
