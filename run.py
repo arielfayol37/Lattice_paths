c6dc5e07b7dcb8da487ed565b6d30d28ac65f765
arielfayol37 <115123676+arielfayol37@users.noreply.github.com>
2022-12-28 14:55:42 -0500

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 #python3 run.py
-import os
+import os,shelve
 import openpyxl,time, concurrent.futures as cf
 from Population import Population
 #from Population import Population
@@ -77,10 +77,12 @@ def run(m,n,k,mode="roulette",old_world=None,pop_size=1000):
     return world
 """
 
-def collect_data(m,n):
+def collect_data_genetic(m,n):
     wb1 = openpyxl.load_workbook("lt_ds_rloo_" + str(n)+".xlsx")
     wb2 = openpyxl.load_workbook("lt_ds_loo_" + str(n)+".xlsx")
     filename = "lattice_table_genetic_" + str(n) + '.xlsx'
+    config_indexes = []
+    shelfFile = shelve.open("evolution_config")
     try:
         wb = openpyxl.load_workbook(filename)
     except:
@@ -89,17 +91,21 @@ def collect_data(m,n):
     for i in range(m,12):
          
         for k in range(3,i+n-1):
-            if wb1["sheet"].cell(i+1,k+1) > wb2["sheet"].cell(i+1,k+1):
-                target = wb1["sheet"].cell(i+1,k+1)
+            if wb1["Sheet"].cell(i+1,k+1).value > wb2["Sheet"].cell(i+1,k+1).value:
+                target = wb1["Sheet"].cell(i+1,k+1).value
             else:
-                target = wb2["sheet"].cell(i+1,k+1)   
+                target = wb2["Sheet"].cell(i+1,k+1).value   
              
-            run = parallel_search(target=target,m=i,n=n,k=k)
+            run, ci = parallel_search(target=target,m=i,n=n,k=k)
+            
             while run:
+                config_indexes.append(ci)
                 target +=1
-                run = parallel_search(target=target,m=i,n=n,k=k)
-            wb["sheet"].cell(i+1,k+1).value = target-1
-            wb.save(filename)    
+                run, ci = parallel_search(target=target,m=i,n=n,k=k)
+                
+            wb["Sheet"].cell(i+1,k+1).value = target-1
+            wb.save(filename)
+            shelfFile['list_of_configs'] = config_indexes    
     wb.close()
     wb1.close()
     wb2.close()
@@ -121,14 +127,15 @@ def parallel_search(target,m,n,k):
     # Set the parameter values for the function
     
     evolution_parameters = [
-         {'size': 1000, 'j': target, 'm': m, 'n': n, 'k': k, 'kill_mode': "non_bias_random",'temp':10},
-        {'size': 400, 'j': target, 'm': m, 'n': n, 'k': k, 'kill_mode': "non_bias_random",'temp':5},
+         {'size': 1000, 'j': target, 'm': m, 'n': n, 'k': k, 'kill_mode': "non_bias_random",'temp':5},
+        {'size': 600, 'j': target, 'm': m, 'n': n, 'k': k, 'kill_mode': "non_bias_random",'temp':10},
         {'size': 5000, 'j': target, 'm': m, 'n': n, 'k': k, 'kill_mode': "non_bias_random",'temp':3},
-        {'size': 20000, 'j': target, 'm': m, 'n': n, 'k': k, 'kill_mode': "kill_bottom",'temp':2},
+        {'size': 20000, 'j': target, 'm': m, 'n': n, 'k': k, 'kill_mode': "non_bias_random",'temp':4},
          
     ]
      
- 
+    shelfFile = shelve.open("genetic_data")
+    config_index = -1
     # Create a ProcessPoolExecutor object to run the function in parallel
     #with cf.ProcessPoolExecutor() as executor:
     with ProcessPool() as executor:
@@ -140,13 +147,16 @@ def parallel_search(target,m,n,k):
 
         # Wait for the tasks to complete
         run = True
-        count = 0 
+        #result = Population(1,1,1,1,1)
+        foo = "population_"+str(m)+"_"+str(n)+"_"+str(k)
         while run:
             for i in range(len(tasks)):
                 if tasks[i].done():
                     
                     result = tasks[i].result()
+                    shelfFile[foo]= result
                     if result.fitnesses[result.bfi] == 9999:
+                        
                         run = False
                         config_index = i                        
                         for other_task in tasks:
@@ -155,47 +165,70 @@ def parallel_search(target,m,n,k):
                              
                         break
             time.sleep(target*10)
-            if count !=0:
-                if result.fitnessees[result.bfi]!=9999:
-                    if tasks[-1].done():
+             
+            if tasks[-1].done():
+                if True:
+                    if shelfFile[foo].fitnesses[shelfFile[foo].bfi]!=9999:
                         #merge all populations
                         new_pop =tasks[0].result()
                         for task in tasks[1:]:
                             new_pop.individuals.append(task.result().individuals)
-                            new_pop.max_size = 4000
+                            new_pop.max_size = 7000
                             new_pop.av_pop_fitnesses.clear()
                             new_pop.av_pop_divergences.clear()
                         new_best = new_pop.evolve(mode="roulette",kill_mode = "non_bias_random")    
+                        run = False
+                        result = new_pop
+                        shelfFile[foo]= result
+                        """
                         if new_pop.fitnesses[new_pop.bfi]==9999:
+        
                             result = new_pop
-                      
+                        """    
+                        
+    result = shelfFile[foo]                  
     result.individuals[result.bfi].show(result.paths)
     f = result.fitnesses[result.bfi]
     print("BestFitness:", f, "Config_index: ", config_index)    
     if f== 9999:
 
-        return True
+        return (True,config_index)
     else:
-        return False    
+        return (False,config_index)    
  
 
-def greedy(m,n,k,pats):
+def greedy(m,n,k,pats, reverse=True):
     sol = []
-    pats.reverse()
-    l = Sequence(m,n,empty=True)
-    l.terms = pats[0]
-    sol.append(l)
-    
-    for pat in pats[1:]:
-        s = Sequence(m,n,empty=True)
-        s.terms = pat
-        for i in range(len(sol)):
-            if s.compare(sol[i],k)==0:
-                break
-            if i == len(sol)-1:
-                sol.append(s)
-    
+    if not reverse:
+        #pats.reverse()
+        l = Sequence(m,n,empty=True)
+        l.terms = pats[0]
+        sol.append(l)
+        
+        for pat in pats[1:]:
+            s = Sequence(m,n,empty=True)
+            s.terms = pat
+            for i in range(len(sol)):
+                if s.compare(sol[i],k)==0:
+                    break
+                if i == len(sol)-1:
+                    sol.append(s)
+    else:
+        l = Sequence(m,n,empty=True)
+        l.terms = pats[-1]
+        sol.append(l)
+        
+        for j in range(len(pats)-1,-1,-1):
+            s = Sequence(m,n,empty=True)
+            s.terms = pats[j]
+            for i in range(len(sol)):
+                if s.compare(sol[i],k)==0:
+                    break
+                if i == len(sol)-1:
+                    sol.append(s)
+
     return len(sol)
+"""   
 def greedy_test(m,n,k,paths,recurse=False):
     solutions = []
     pati = paths.copy()
@@ -213,11 +246,12 @@ def greedy_test(m,n,k,paths,recurse=False):
         
         solutions.append(greedy(m,n,k,patos))
     return solutions
-def greedy_t(m,n,k,paths):
+"""     
+def greedy_t(m,n,k,paths,reverse):
     solutions = []
     pati = paths.copy()
     max_indexes = (-2,-2)
-    max = greedy(m,n,k,pati)
+    max = greedy(m,n,k,pati,reverse)
     solutions.append(max)
     for i in range(len(paths)):
         patos = paths.copy()#paths, not pati because pati has been reversed inside greedy()
@@ -225,7 +259,7 @@ def greedy_t(m,n,k,paths):
         for j in range(len(patos)):
             patz = patos.copy()
             patz.pop(j)
-            amax = greedy(m,n,k,patz)
+            amax = greedy(m,n,k,patz,reverse)
             if amax>max:
                 max = amax
                 if j>=i:
@@ -235,7 +269,7 @@ def greedy_t(m,n,k,paths):
             solutions.append(max)
         
         assert len(patos) == len(paths)-1
-        pmax = greedy(m,n,k,patos)
+        pmax = greedy(m,n,k,patos,reverse)
         if pmax > max:
             max=pmax
             max_indexes = (i,-1)
@@ -249,31 +283,52 @@ def find_max(alist):
             maxs = item
     return maxs
 def collect_data_greedy(m,n):
-    filename = "lattice_table_greedy_double_slicing_rloo" + str(n) + '.xlsx'
+    filename1 = "lattice_table_greedy_double_slicing_rloo" + str(n) + '.xlsx'
     try:
-        wb = openpyxl.load_workbook(filename)
+        wb1 = openpyxl.load_workbook(filename1)
     except:
-        wb = openpyxl.Workbook()
-    sheet = wb['Sheet']
-    sheet.cell(1,1).value ="m/k"
+        wb1 = openpyxl.Workbook()
+    filename2 = "lattice_table_greedy_double_slicing_loo" + str(n) + '.xlsx'
+    try:
+        wb2 = openpyxl.load_workbook(filename2)
+    except:
+        wb2 = openpyxl.Workbook()    
+    sheet1 = wb1['Sheet']
+    sheet1.cell(1,1).value ="m/k"
+    sheet2 = wb2['Sheet']
+    sheet2.cell(1,1).value ="m/k"
 
     for i in range(m,12):
         paths = generate_all_paths(i,n)
          
         for k in range(3,i+n-1):
-            sheet.cell(1,k+1).value =str(k)
-            sheet.cell(i+1,1).value= str(i)
-            sheet.cell(1+15,2*k+1).value =str(k)
-            sheet.cell(i+1+15,1).value= str(i)
-            f, max_indexes = greedy_t(i,n,k,paths)
+            sheet1.cell(1,k+1).value =str(k)
+            sheet1.cell(i+1,1).value= str(i)
+            sheet1.cell(1+15,2*k+1).value =str(k)
+            sheet1.cell(i+1+15,1).value= str(i)
+            f1, max_indexes1 = greedy_t(i,n,k,paths,True)
            
-            maxi = find_max(f)
-            sheet.cell(i+1,k+1).value=maxi
-            sheet.cell(i+1+15,2*k+1).value=max_indexes[0]
-            sheet.cell(i+1+15,2*k+2).value=max_indexes[1]
+            maxi1 = find_max(f1)
+            sheet1.cell(i+1,k+1).value=maxi1
+            sheet1.cell(i+1+15,2*k+1).value=max_indexes1[0]
+            sheet1.cell(i+1+15,2*k+2).value=max_indexes1[1]
 
              
-            wb.save(filename)
+            wb1.save(filename1)
+
+            sheet2.cell(1,k+1).value =str(k)
+            sheet2.cell(i+1,1).value= str(i)
+            sheet2.cell(1+15,2*k+1).value =str(k)
+            sheet2.cell(i+1+15,1).value= str(i)
+            f2, max_indexes2 = greedy_t(i,n,k,paths,False)
+           
+            maxi2 = find_max(f2)
+            sheet2.cell(i+1,k+1).value=maxi2
+            sheet2.cell(i+1+15,2*k+1).value=max_indexes2[0]
+            sheet2.cell(i+1+15,2*k+2).value=max_indexes2[1]
+
+             
+            wb2.save(filename2)
 
 def nbrr(non_bias_random_times):
 
@@ -328,3 +383,11 @@ def test(size,j,m,n,k,kill_mode="non_bias_random",mode="roulette",norm=True, sca
     if visualize:    
         world.visualize_evolution()
     return  
+
+"""
+if __name__ =="__main__":
+     
+    for i in [3,4,5]:
+        collect_data_greedy(i,i)
+        collect_data_genetic(i,i)
+"""
